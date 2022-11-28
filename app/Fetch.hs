6f1480fbea536ece27065dd91a54979ac438922d
Love Lyckaro <love.lyckaro@hotmail.se>
2022-11-28 20:11:31 +0100

    Update usage messages for fetch and submit

--- app/Fetch.hs
+++ app/Fetch.hs
@@ -1,14 +1,17 @@
-{-# LANGUAGE TypeApplications #-}
 module Main where
 import SantaLib
 import System.Environment
+import System.Exit (exitFailure)
+
+printUsage :: IO a
+printUsage = putStrLn "To fetch description, your input, and (hopefully) the example input for day n:\ncabal run fetch n" >> exitFailure
 
 main :: IO ()
 main = do
   env <- getArgs
   day <- case env of
-    [dayStr] -> return $ read @Integer dayStr
-    _ -> error "No day submitted"
+    [dayStr] -> return $ read @Integer dayStr
+    _ -> printUsage
   putStrLn $ "Fetching day " <> show day <> "..."
   putStrLn "Fetching Description"
   fetchDescription day
--- app/Submit.hs
+++ app/Submit.hs
@@ -1,12 +1,15 @@
-{-# LANGUAGE TypeApplications #-}
 module Main where
 import SantaLib
 import System.Environment
+import System.Exit (exitFailure)
+
+printUsage :: IO a
+printUsage = putStrLn "To submit solution for day n part p:\ncabal run submit n p" >> exitFailure
 
 main :: IO ()
 main = do
   env <- getArgs
   (day, partInt) <- case env of
     [dayStr, partString] -> return (read @Integer dayStr, read @Int partString)
-    _ -> error "Usage cabal run submit [day in 1..25] [part in 1..2]" 
-  submitAnswer day (toEnum $ partInt - 1)
+    _ -> printUsage
+  submitAnswer day (toEnum $ partInt - 1)
