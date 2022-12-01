e139b58070aaa1c40afaf8a763b654f4e44ac9e6
Love Lyckaro <love.lyckaro@hotmail.se>
2022-12-01 08:13:55 +0100

    Finish day 1

--- app/Day01.hs
+++ app/Day01.hs
@@ -1,7 +1,21 @@
 module Main where
+
+import Data.List (sortOn)
+import Data.List.Split (splitOn)
+import Data.Ord (Down (Down))
 import SantaLib
+
+pInp :: String -> [[Int]]
+pInp = map (map read . lines) . splitOn "\n\n"
+
+part1 :: String -> Int
+part1 = maximum . map sum . pInp
+
+part2 :: String -> Int
+part2 = sum . take 3 . sortOn Down . map sum . pInp
 
 main :: IO ()
 main = do
   inp <- getInput 1
-  putStrLn "day 1 is not yet implemented"
+  putAnswer 1 Part1 (part1 inp)
+  putAnswer 1 Part2 (part2 inp)
