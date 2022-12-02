0571126542ecbfb0b0d4bdc26f205c6b12603b14
Love Lyckaro <love.lyckaro@hotmail.se>
2022-12-02 06:42:20 +0100

    Clean up solution 2 somewhat

--- app/Day02.hs
+++ app/Day02.hs
@@ -1,6 +1,7 @@
 module Main where
-import SantaLib
+
 import Data.Maybe (fromJust)
+import SantaLib
 
 data P2Input = X | Y | Z
   deriving (Show, Eq, Ord, Read)
@@ -13,37 +14,33 @@ choicePoints X = 1
 choicePoints Y = 2
 choicePoints Z = 3
 
-p2wins :: [(P1Input, P2Input)]
+p2wins, p2draws, p2loses :: [(P1Input, P2Input)]
 p2wins = [(A, Y), (B, Z), (C, X)]
-
 p2draws = [(A, X), (B, Y), (C, Z)]
-
 p2loses = [(A, Z), (B, X), (C, Y)]
 
-score :: P1Input -> P2Input -> Int
-score p1 p2
-  | (p1, p2) `elem` p2wins = 6 + choicePoints p2
-  | (p1, p2) `elem` p2draws = 3 + choicePoints p2
-  | (p1, p2) `elem` p2loses = 0 + choicePoints p2
+score :: (P1Input, P2Input) -> Int
+score game
+  | game `elem` p2wins = 6 + choicePoints (snd game)
+  | game `elem` p2draws = 3 + choicePoints (snd game)
+  | game `elem` p2loses = 0 + choicePoints (snd game)
+
+pInp :: String -> [(P1Input, P2Input)]
+pInp = map pLine . lines
+  where
+    pLine l = case words l of
+      [p1, p2] -> (read p1, read p2)
 
 part1 :: String -> Int
-part1 = sum . map (uncurry score . pLine) . lines
-  where
-    pLine l = case words l of
-      [p1, p2] -> (read p1, read p2)
+part1 = sum . map score . pInp
 
-part2Score :: P1Input -> P2Input -> Int
-part2Score p1 X = 0 + choicePoints (fromJust (lookup p1 p2loses))
-part2Score p1 Y = 3 + choicePoints (fromJust (lookup p1 p2draws))
-part2Score p1 Z = 6 + choicePoints (fromJust (lookup p1 p2wins))
-
+part2Score :: (P1Input, P2Input) -> Int
+part2Score (p1, X) = 0 + choicePoints (fromJust (lookup p1 p2loses))
+part2Score (p1, Y) = 3 + choicePoints (fromJust (lookup p1 p2draws))
+part2Score (p1, Z) = 6 + choicePoints (fromJust (lookup p1 p2wins))
 
 part2 :: String -> Int
-part2 = sum . map (uncurry part2Score . pLine) . lines
-  where
-    pLine l = case words l of
-      [p1, p2] -> (read p1, read p2)
-
+part2 = sum . map part2Score . pInp
 
 main :: IO ()
 main = do
