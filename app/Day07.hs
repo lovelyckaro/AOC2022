cf464f040f406d3367edf2fde745f57e8f26b45b
Love Lyckaro <love.lyckaro@hotmail.se>
2022-12-07 09:29:45 +0100

    remake day7 in terms of mkdir and touch

--- app/Day07.hs
+++ app/Day07.hs
@@ -10,15 +10,19 @@ import System.Exit (exitFailure)
 data FileTree = Dir (Map String FileTree) | File Int
   deriving (Show)
 
+-- |  A bread crumb saves which directory we've entered, as well as the other
+--  directories and files.
 data BreadCrumb = Crumb String (Map String FileTree)
   deriving (Show)
 
-type FileTreeZipper = (Map String FileTree, [BreadCrumb])
+-- | Zipper for File Tree, keeps current directory contents, as well as bread
+-- crumbs for path taken
+type Location = (Map String FileTree, [BreadCrumb])
 
 data Path = ChildDir String | Root | ToParent
   deriving (Show)
 
-data Cmd = Cd Path | Ls (Map String FileTree)
+data Cmd = Cd Path | Touch String Int | MkDir String
   deriving (Show)
 
 pPath :: Parser Path
@@ -28,60 +32,51 @@ pPath = pRoot <|> pParent <|> pChildDir
     pParent = ToParent <$ symbol ".."
     pChildDir = ChildDir <$> lexeme (some printChar)
 
-pLSResult :: Parser (Map String FileTree)
-pLSResult = do
-  results <- some $ pDir <|> pFile
-  return $ M.fromList results
+pLSResult :: Parser [Cmd]
+pLSResult = some $ pDir <|> pFile
   where
-    pDir = do
-      symbol "dir"
-      n <- lexemeLn (some printChar)
-      pure (n, Dir M.empty)
+    pDir = symbol "dir" >> MkDir <$> lexemeLn (some printChar)
     pFile = do
-      f <- File <$> lexeme decimal
+      f <- lexeme decimal
       n <- lexemeLn (some printChar)
-      pure (n, f)
+      pure (Touch n f)
 
-pCommand :: Parser Cmd
+pCommand :: Parser [Cmd]
 pCommand = symbol "$" >> (pCd <|> pLs)
   where
-    pCd = Cd <$> (symbol "cd" >> lexemeLn pPath)
-    pLs = Ls <$> (symbolLn "ls" >> pLSResult)
+    pCd = (:[]) . Cd <$> (symbol "cd" >> lexemeLn pPath)
+    pLs = symbolLn "ls" >> pLSResult
 
 pInp :: Parser [Cmd]
-pInp = some pCommand
+pInp = concat <$> some pCommand
 
-root :: FileTreeZipper
+root :: Location
 root = (M.empty, [])
 
-cd :: Path -> FileTreeZipper -> Maybe FileTreeZipper
+cd :: Path -> Location -> Maybe Location
 cd Root (dirContents, parents) = case parents of
   [] -> Just (dirContents, [])
-  _ -> let Crumb _ rootContents = last parents in Just (rootContents, [])
+  _ -> cd ToParent (dirContents, parents) >>= cd Root
 cd ToParent (dirContents, parents) = case parents of
   [] -> Nothing
-  (Crumb _ parentContents : grandParents) -> Just (parentContents, grandParents)
+  (Crumb name parentContents : grandParents) -> Just (M.insert name (Dir dirContents) parentContents, grandParents)
 cd (ChildDir name) (dirContents, parents) = do
   (Dir contents) <- dirContents M.!? name
-  Just (contents, Crumb name dirContents : parents)
+  let dirContents' = M.delete name dirContents
+  Just (contents, Crumb name dirContents' : parents)
 
-update :: Map String FileTree -> [BreadCrumb] -> Maybe [BreadCrumb]
-update m [] = Just []
-update newContents (Crumb chosen parentContents : crumbs) = do
-  (Dir knownContents) <- parentContents M.!? chosen
-  let mergedContents = M.union newContents knownContents
-  let crumb'@(Crumb _ pContents') = Crumb chosen (M.insert chosen (Dir mergedContents) parentContents)
-  crumbs' <- update pContents' crumbs
-  return (crumb' : crumbs')
+mkdir :: String -> Location -> Location
+mkdir name (dirContents, parents) = (M.insert name (Dir M.empty) dirContents, parents)
 
-walk :: [Cmd] -> FileTreeZipper -> Maybe FileTreeZipper
-walk [] zipper = Just zipper
-walk (Ls dirContents : rest) (knownContents, parents) = do
-  let contents' = M.union dirContents knownContents
-  parents' <- update contents' parents
-  walk rest (contents', parents')
-walk (Cd path : rest) ft = do
-  ft' <- cd path ft
+touch :: String -> Int -> Location -> Location
+touch name size (dirContents, parents) = (M.insert name (File size) dirContents, parents)
+
+walk :: [Cmd] -> Location -> Maybe Location
+walk [] location = Just location
+walk (MkDir name : rest) location = walk rest $ mkdir name location
+walk (Touch name size : rest) location = walk rest $ touch name size location
+walk (Cd path : rest) location = do
+  ft' <- cd path location
   walk rest ft'
 
 size :: FileTree -> Int
