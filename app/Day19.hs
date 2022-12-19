0d2a505bf8d8e77173e1652e3d0a9432ac9721bb
Love Lyckaro <love.lyckaro@hotmail.se>
2022-12-19 15:51:59 +0100

    Add enough explanation for me to understand in a year

--- app/Day19.hs
+++ app/Day19.hs
@@ -63,6 +63,22 @@ canAfford bp robotMaterial = do
   let afford = sAnd $ M.elems $ M.intersectionWith (.>=) available cost
   return afford
 
+{-
+The problem is modeled by introducing four boolean choices each minute (One for each buying each type of robot).
+As well as keeping track of (symbolic) 8 bit values representing the current
+amount of resources of each type, as well as the amount of robots of each type.
+
+Each minute is divided into three phases:
+
+First the buying phase:
+We are only allowed to buy a robot if we can afford it. We are only allowed to
+buy one robot per minute. If we buy a robot the cost of that robot is subtracted
+from our available resources.
+
+Then the mining phase. This is just adding materials to our available resources.
+
+Finally in the last phase any robot we bought is added to the available robots.
+-}
 evalMinute :: Blueprint -> RobotM ()
 evalMinute blueprint = do
   -- Buy phase
@@ -73,6 +89,7 @@ evalMinute blueprint = do
     affordable <- canAfford blueprint material
     -- must afford the robot to buy
     lift $ constrain $ shouldBuy .=> affordable
+    -- subtract the cost of the robot if we buy
     let cost = M.map (* oneIf shouldBuy) $ blueprint.robots M.! material
     modify $ \s -> s {materials = M.unionWith (-) (materials s) cost}
   -- Mine phase
@@ -95,12 +112,6 @@ mkObjective numMinutes bp = do
   finalState <- run $ replicateM_ numMinutes (evalMinute bp)
   let numGeodes = finalState.materials M.! Geode
   maximize "goal" numGeodes
-  finalResources <- sWord8s ["Final ore", "Final clay", "Final obsidian", "Final geode"]
-  forM_ (zip [Ore ..] finalResources) $ \(resource, var) ->
-    constrain $ var .== finalState.materials M.! resource
-  finalIncome <- sWord8s ["Final ore robots", "Final clay robots", "Final obsidian robots", "Final geode robots"]
-  forM_ (zip [Ore ..] finalIncome) $ \(resource, var) ->
-    constrain $ var .== finalState.income M.! resource
 
 optimalGeodes :: Int -> Blueprint -> IO Word8
 optimalGeodes numMinutes bp = do
